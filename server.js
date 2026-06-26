36851d07f4345b8db41ae2e1103113320b134fe1
PartyPack2026 <klekbom.music@gmail.com>
2026-06-26 21:00:55 +1000

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  transports: ["websocket", "polling"],
+  pingTimeout: 60000,
+  pingInterval: 25000
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
